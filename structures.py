4c1dd6770592852588027eb428aa57aae3d5fdc8
ojasJO <ojasjjo@gmail.com>
2025-12-23 21:24:24 +0530

    Update structures.py

--- structures.py
+++ structures.py
@@ -8,27 +8,34 @@ class BondState(Enum):
     BLOCKED = auto()
     CONFIRMED = auto()
 
+class StrategyLevel(Enum):
+    LEVEL_1_BASIC = "Uniqueness (Basic)"
+    LEVEL_2_STANDARD = "Isolation (Standard)"
+    LEVEL_3_ADVANCED = "Duplicate Check (Advanced)"
+    LEVEL_4_ALL = "Full Power (All Strategies)"
+
 
 @dataclass
 class CellNode:
     r: int
     c: int
     value: int
-    neighbors: List["CellNode"] = field(default_factory=list)
+    neighbors: List['CellNode'] = field(default_factory=list)
     occupied: bool = False
 
     def __repr__(self):
-        return f"Cell({self.r}, {self.c} | Val: {self.value})"
-
+        return f"{self.value}"
 
 @dataclass
 class EdgeBond:
     node_a: CellNode
     node_b: CellNode
     state: BondState = BondState.UNDECIDED
-
+    
     def get_pair_id(self) -> Tuple[int, int]:
-        return tuple(sorted((self.node_a.value, self.node_b.value)))
+
+        return tuple(sorted((int(self.node_a.value), int(self.node_b.value))))
 
     def __repr__(self):
-        return f"Bond[{self.state.name}]({self.node_a} <-> {self.node_b})"
+        v1, v2 = self.get_pair_id()
+        return f"[{v1}|{v2}]"
