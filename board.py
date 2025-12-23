f09475f4176a43593eda20bbf29172c6571a5c85
ojasJO <ojasjjo@gmail.com>
2025-12-23 21:19:12 +0530

    Update board.py

--- board.py
+++ board.py
@@ -1,43 +1,46 @@
 import numpy as np
 from structures import CellNode, EdgeBond, BondState
 
-
 class DominosaBoard:
     def __init__(self, matrix: np.ndarray):
         self.rows, self.cols = matrix.shape
         self.grid = matrix
-        self.cells = [
-            [CellNode(r, c, matrix[r, c]) for c in range(self.cols)]
-            for r in range(self.rows)
-        ]
+        self.cells = []
         self.edges = []
         self.possible_moves = {}
+        
+        self._init_cells()
         self._init_graph_connections()
         self._init_constraints()
+
+    def _init_cells(self):
+        for r in range(self.rows):
+            row_cells = []
+            for c in range(self.cols):
+                row_cells.append(CellNode(r, c, self.grid[r, c]))
+            self.cells.append(row_cells)
 
     def _init_graph_connections(self):
         for r in range(self.rows):
             for c in range(self.cols - 1):
-                a = self.cells[r][c]
-                b = self.cells[r][c + 1]
-                bond = EdgeBond(a, b)
-                self.edges.append(bond)
-                a.neighbors.append(b)
-                b.neighbors.append(a)
-
+                self._create_bond(self.cells[r][c], self.cells[r][c+1])
+        
         for r in range(self.rows - 1):
             for c in range(self.cols):
-                a = self.cells[r][c]
-                b = self.cells[r + 1][c]
-                bond = EdgeBond(a, b)
-                self.edges.append(bond)
-                a.neighbors.append(b)
-                b.neighbors.append(a)
+                self._create_bond(self.cells[r][c], self.cells[r+1][c])
+
+    def _create_bond(self, node_a, node_b):
+        bond = EdgeBond(node_a, node_b)
+        self.edges.append(bond)
+        node_a.neighbors.append(node_b)
+        node_b.neighbors.append(node_a)
 
     def _init_constraints(self):
         for edge in self.edges:
             pid = edge.get_pair_id()
-            self.possible_moves.setdefault(pid, []).append(edge)
+            if pid not in self.possible_moves:
+                self.possible_moves[pid] = []
+            self.possible_moves[pid].append(edge)
 
     def get_cell(self, r, c):
         return self.cells[r][c]
