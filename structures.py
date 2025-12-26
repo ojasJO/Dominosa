769ad5581ca63034512e52f5d393d4574ce31142
ojasJO <ojasjjo@gmail.com>
2025-12-26 13:34:28 +0530

    Update structures.py

--- structures.py
+++ structures.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass, field
 from enum import Enum, auto
 from typing import List, Tuple
 
-
 class BondState(Enum):
     UNDECIDED = auto()
     BLOCKED = auto()
@@ -13,7 +12,6 @@ class StrategyLevel(Enum):
     LEVEL_2_STANDARD = "Isolation (Standard)"
     LEVEL_3_ADVANCED = "Duplicate Check (Advanced)"
     LEVEL_4_ALL = "Full Power (All Strategies)"
-
 
 @dataclass
 class CellNode:
@@ -33,8 +31,15 @@ class EdgeBond:
     state: BondState = BondState.UNDECIDED
     
     def get_pair_id(self) -> Tuple[int, int]:
+    
+        v1 = int(self.node_a.value)
+        v2 = int(self.node_b.value)
 
-        return tuple(sorted((int(self.node_a.value), int(self.node_b.value))))
+
+        if v1 > v2:
+            return (v2, v1)
+        else:
+            return (v1, v2)
 
     def __repr__(self):
         v1, v2 = self.get_pair_id()
