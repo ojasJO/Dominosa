620a4b0e34851fd939868d2bf8479e2dd79dc0d7
ojasJO <ojasjjo@gmail.com>
2026-02-18 22:11:01 +0530

    Update board.py
    numpy matrix -> python matrix 
    possible_moves dict -> available_dominoes set 
    placement tracking introduced 
    unified topology builder

--- board.py
+++ board.py
@@ -1,46 +1,63 @@
-import numpy as np
 from structures import CellNode, EdgeBond, BondState
+from typing import List, Tuple, Set
 
 class DominosaBoard:
-    def __init__(self, matrix: np.ndarray):
-        self.rows, self.cols = matrix.shape
-        self.grid = matrix
-        self.cells = []
-        self.edges = []
-        self.possible_moves = {}
+    def __init__(self, matrix: List[List[int]]):
+        # numpy shape logic completely changed
+        self.rows = len(matrix)
+        self.cols = len(matrix[0])
+        self.matrix_data = matrix
 
-        self._init_cells()
-        self._init_graph_connections()
-        self._init_constraints()
+        # glid initilaized proeprly (preiviious one used to bug when algorithm was chnaged midgame)  
+        self.cells = [
+            [CellNode(r, c, val) for c, val in enumerate(row)]
+            for r, row in enumerate(matrix)
+        ]
 
-    def _init_cells(self):
+        self.edges: List[EdgeBond] = []
+
+        # NEW -> domino tracking system introduced 
+        
+        self.available_dominoes: Set[Tuple[int, int]] = set()
+        self.placed_dominoes: Set[Tuple[int, int]] = set()
+
+        self._init_topology()
+        self._init_domino_set()
+
+        self.total_dominoes = len(self.available_dominoes)
+
+    def _init_topology(self):
+        
         for r in range(self.rows):
-            row_cells = []
             for c in range(self.cols):
-                row_cells.append(CellNode(r, c, self.grid[r, c]))
-            self.cells.append(row_cells)
+                curr = self.cells[r][c]
 
-    def _init_graph_connections(self):
-        for r in range(self.rows):
-            for c in range(self.cols - 1):
-                self._create_bond(self.cells[r][c], self.cells[r][c + 1])
+                if c + 1 < self.cols:
+                    self._create_bond(curr, self.cells[r][c + 1])
 
-        for r in range(self.rows - 1):
-            for c in range(self.cols):
-                self._create_bond(self.cells[r][c], self.cells[r + 1][c])
+                if r + 1 < self.rows:
+                    self._create_bond(curr, self.cells[r + 1][c])
 
     def _create_bond(self, node_a, node_b):
         bond = EdgeBond(node_a, node_b)
         self.edges.append(bond)
+
+        # Keep neighbor tracking
         node_a.neighbors.append(node_b)
         node_b.neighbors.append(node_a)
 
-    def _init_constraints(self):
-        for edge in self.edges:
-            pid = edge.get_pair_id()
-            if pid not in self.possible_moves:
-                self.possible_moves[pid] = []
-            self.possible_moves[pid].append(edge)
+    def _init_domino_set(self):
+
+        if not self.cells:
+            return
+
+        max_val = max(max(cell.value for cell in row) for row in self.cells)
+
+        self.available_dominoes = {
+            (i, j)
+            for i in range(max_val + 1)
+            for j in range(i, max_val + 1)
+        }
 
     def get_cell(self, r, c):
         return self.cells[r][c]
